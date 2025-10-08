41cb758cea546efcd1b8babcf78e5a86266508a6
BHAWANISHANKARMARU <Bhawanishankarmaru2005@gmail.com>
2025-10-08 16:50:53 +0530

    animate  changes

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -28,7 +28,7 @@ const InfoSection = () => {
       className={styles.infoSection}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: true, amount: 0.4 }}
+      viewport={{ once: true, amount: 0.1 }}
       transition={{ duration: 0.7, ease: "easeOut" }}
       variants={containerVariants}
     >
--- src/components/LearningPath.js
+++ src/components/LearningPath.js
@@ -28,7 +28,7 @@ const LearningPath = () => {
       className={styles.learningPath}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: true, amount: 0.4 }}
+      viewport={{ once: true, amount: 0.1 }}
       transition={{ duration: 0.7, ease: "easeOut" }}
       variants={containerVariants}
     >
