80aef5118e3e20fd1b5e15aa559d906de7940442
BHAWANISHANKARMARU <Bhawanishankarmaru2005@gmail.com>
2025-10-08 18:20:38 +0530

    md-2  change

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -4,8 +4,10 @@ import BlogPostClient from '@/components/BlogPostClient';
 // This function gets called at build time on server-side.
 // It won't be called on client-side, so you can import server-side modules
 export async function generateStaticParams() {
-  const paths = getAllPostIds();
-  return paths;
+  const postIds = getAllPostIds();
+  return postIds.map((post) => ({
+    slug: post.params.slug,
+  }));
 }
 
 export default async function BlogPost({ params }) {
